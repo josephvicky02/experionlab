875703988e9c439a5c663f1eaa6c798f539467d8
josephvicky02 <121211638+josephvicky02@users.noreply.github.com>
2023-01-05 11:34:13 +0530

    Add files via upload

---
+++ 03-01-2023/new.js
@@ -0,0 +1,18 @@
+const student = [
+    {
+        name: 'Ab',
+        age: 31,
+        mark1: 397,
+        mark2: 379
+    },
+    {
+        name: 'bc',
+        age: 26,
+        mark1:426,
+        mark2:357
+    }
+];
+
+
+const data = JSON.stringify(student);
+console.log(data)
